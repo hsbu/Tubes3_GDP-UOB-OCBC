d870fa0bfb59583e52e28b96a9cfba7e03b93e83
Muhamad Hasbullah Faris <18223014@std.stei.itb.ac.id>
2026-05-23 22:59:26 +0700

    chore: add algo feature flags for modular development

---
+++ src/shared/config.ts
@@ -0,0 +1,10 @@
+// Made to help the development of feature without dependency to algorithm
+// Flag to True if implemented
+export const ALGO_FLAGS = {
+  kmp: false,
+  bm: false,
+  regex: true,
+  levenshtein: false,
+  ahoCorasick: false,
+  rabinKarp: false,
+} as const;
